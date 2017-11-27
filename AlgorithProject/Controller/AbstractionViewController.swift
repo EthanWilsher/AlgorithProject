77139be07be3315be2bdd6140bfa5682c90502be
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-11-27 08:32:14 -0700

    Added the swipe left method

--- AlgorithProject/Controller/AbstractionViewController.swift
+++ AlgorithProject/Controller/AbstractionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
 {
+    //MARK: Array of subtitles
     private (set) lazy var orderedAbstractionViews : [UIViewController] =
     {
         return[
@@ -20,12 +21,52 @@ public class AbstractionViewController: UIPageViewController, UIPageViewControll
             self.newAbstractionViewController(abstractionLevel: "AndGate"),
         ]
     }()
+ 
     
-    private func newAbstractionViewController(abstractionLevel : String) -> UIViewControll
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
     {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
     }
+    
 
     override public func viewDidLoad()
     {
-    
+        super.viewDidLoad()
+        dataSource = self
+            if let firstViewController = orderedAbstractionViews.first
+            {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
+    //MARK:- Required Protocol methods for UIPageViewControllerDataSource
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+        
+            }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter
+
+}
