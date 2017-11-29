2123f70b3d6037c7e5bb05706c1361589037d133
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-11-29 07:50:25 -0700

    I added a split view controller and 2 internet view files.

---
+++ AlgorithProject/Controller/InternetDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InternetDetailViewController.swift
+//  AlgorithProject
+//
+//  Created by Wilsher, Ethan on 11/29/17.
+//  Copyright © 2017 Wilsher, Ethan. All rights reserved.
+//
+
+import UIKit
+
+class InternetDetailViewController: NSObject {
+
+}
---
+++ AlgorithProject/Controller/InternetMasterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InternetMasterViewController.swift
+//  AlgorithProject
+//
+//  Created by Wilsher, Ethan on 11/29/17.
+//  Copyright © 2017 Wilsher, Ethan. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterViewController: NSObject {
+
+}
