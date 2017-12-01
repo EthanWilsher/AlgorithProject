bcfc0b0600cf2ac9aa791a71b754e26c0ae7e57f
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-12-01 09:23:23 -0700

    Connected some outlets and added some URL methods.

--- AlgorithProject/Controller/InternetDetailViewController.swift
+++ AlgorithProject/Controller/InternetDetailViewController.swift
@@ -8,18 +8,75 @@
 
 import UIKit
 
-class InternetDetailViewController: UIViewController
+public class InternetDetailViewController: UIViewController
 {
     
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var webViewer: WKWebView!
+    
+    
+    
+    var detailAddress : String?
+    {
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    
+    var detailText : String?
+    {
+        didSet
+        {
+            configureDetailView()
+        }
+    }
     
     override public func viewDidLoad()
     {
         super.viewDidLoad()
-        setup()
+        configureDetailView()
     }
     
-    private func setup() -> Void
+    private func configureDetailView() -> Void
     {
+        if detailAddress != nil
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+            
+            
+        }
+        else
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: "https://www.google.com")
+                currentWebView.load(URLRequest(url:currentURL!))
+            }
+        }
+        if detailText != nil
+        {
+            if let currentText = textView
+            {
+                currentText.text = detailText
+            }
+        }
+        else
+        {
+            if let currentText = textView
+            {
+                currentText.text = "Ethans CSP internet app screen"
+            }
+        }
+        
+        
+        
+        
         
     }
 
--- AlgorithProject/Controller/InternetMasterViewController.swift
+++ AlgorithProject/Controller/InternetMasterViewController.swift
@@ -23,22 +23,28 @@ public class InternetMasterViewController: UITableViewController
         
         
     }()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private lazy var addresses : [String] = []
     
     private var detailViewController : InternetDetailViewController?
     
     private func setup() -> Void
     {
+        //TODO: replace with correct links
+        addresses = [
+        "https://www.google.com",
+        "https://www.google.com",
+        "https://www.google.com",
+        "https://www.google.com",
+        "https://www.google.com",
+        "https://www.google.com",
+        ]
         
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as? InternetDetailViewController
+            
+        }
     }
     
     override public func viewDidLoad()
