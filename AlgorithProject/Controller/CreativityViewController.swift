6e6a30c157f8213c6384ef25712154294a6e0983
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-12-07 08:12:31 -0700

    Added code that lets us drop stuff in easier into the creativity view controller.

--- AlgorithProject/Controller/CreativityViewController.swift
+++ AlgorithProject/Controller/CreativityViewController.swift
@@ -8,6 +8,59 @@
 
 import UIKit
 
-class CreativityViewController: NSObject {
+public class CreativityViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
+{
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
+    
+    private lazy var artSelection : [UIImage?] =
+    {
+        return[
+            UIImage(named:"MyArt"),
+            UIImage(named:"MyArt"),
+            UIImage(named:"MyArt"),
+            UIImage(named:"MyArt"),
+            UIImage(named:"MyArt"),
+            UIImage(named:"Haiku"),
+            UIImage(named:"Haiku"),
+            UIImage(named:"Haiku"),
+            UIImage(named:"Haiku"),
+            UIImage(named:"Haiku"),
+            ]
+    }()
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            
+            collectionView?.performBatchUpdates(
+                {
+                    self.collectionView?.reloadItems(at:indexPaths)
+            })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
+                    self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                      at: .centeredVertically,
+                                                      animated: true)
+                }
+                
+            }
+            
+        }
+    }
 
 }
