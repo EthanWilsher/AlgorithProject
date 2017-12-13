dc8a3ff6dfc70b6b1f88bae4b6447138625e0a82
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-12-13 08:53:50 -0700

    changed segue to game and added view controller.

--- AlgorithProject/Controller/ProgrammingViewController.swift
+++ AlgorithProject/Controller/ProgrammingViewController.swift
@@ -7,7 +7,24 @@
 //
 
 import UIKit
+import SpriteKit
 
-class ProgrammingViewController: NSObject {
+class ProgrammingViewController: UIViewController
+{
+    //MARK: UI COMPONENTS
+    
+    override public func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        let gameScene = StartScene(size: view.bounds.size)
+        let gameView = view as! SKView
+        
+        gameView.showsFPS = true
+        gameView.showsNodeCount = true
+        gameView.ignoresSiblingOrder = true
+        
+        gameScene.scaleMode = .resizeFill
+        gameView.presentScene(gameScene)
+    }
 
 }
