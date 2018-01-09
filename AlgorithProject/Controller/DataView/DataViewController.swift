4d619795bb4bcf49ba636ce93de1c073bd314697
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-01-09 08:05:08 -0700

    finished the data view project

--- AlgorithProject/Controller/DataView/DataViewController.swift
+++ AlgorithProject/Controller/DataView/DataViewController.swift
@@ -8,6 +8,74 @@
 
 import UIKit
 
-class DataViewController: UITableViewController {
+class DataViewController: UITableViewController
+{
+    lazy var bucketList : [BucketItem] =
+    {
+       return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() ->[BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = int.components(seperatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(seperatedBy: ",")
+                    items.append(BucketItem(contents: item[0],author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File Load Error")
+            }
+        }
+        return items
+    }
+    
+    //MARK: TableView Code
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection secction: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        //          currentCell.bucketItemSignature.text = currentCell.bucketItem.itemAuthor
+        
+        return currentCell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
